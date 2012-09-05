7d23a5896e8d542f4e4f087fd60d769a3608fbaf
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2012-09-05 23:20:52 +0200

    docs

--- Gedcom/GCObject.h
+++ Gedcom/GCObject.h
@@ -89,6 +89,7 @@
 /// The GCTag corresponding to the receiver's type.
 @property (readonly) GCTag *gedTag;
 
+/// The root object of a given tree of GCObjects. Will usually be a GCEntity.
 @property (readonly) GCObject *rootObject;
 
 /// The receiver as a GCNode.  Setting this property will cause the receiver to interpret the node and add new properties and remove those that no longer exist.
