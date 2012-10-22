1bfc097a3b48edc9f2ead1863d0c682e09c7a352
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2012-10-22 10:35:28 +0200

    GCProperty: dont re-alloc on tag.isCustom

--- Gedcom/GCProperty.m
+++ Gedcom/GCProperty.m
@@ -28,7 +28,7 @@
     GCTag *tag = [object.gedTag subTagWithCode:node.gedTag type:([node valueIsXref] ? @"relationship" : @"attribute")];
     
     if (tag.isCustom) {
-        self = [[tag.objectClass alloc] _initWithType:tag.name];
+        self = [self _initWithType:tag.name];
     } else {
         self = [self init];
     }
