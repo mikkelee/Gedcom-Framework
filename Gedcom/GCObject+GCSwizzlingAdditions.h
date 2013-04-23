912535d1b1a90571c81e85def0d78e73d2a93e6b
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2013-04-23 16:10:33 +0200

    moved swizzle to separate file

---
+++ Gedcom/GCObject+GCSwizzlingAdditions.h
@@ -0,0 +1,13 @@
+//
+//  GCObject+GCSwizzlingAdditions.h
+//  Gedcom
+//
+//  Created by Mikkel Eide Eriksen on 23/04/13.
+//  Copyright (c) 2013 Mikkel Eide Eriksen. All rights reserved.
+//
+
+#import <Gedcom/Gedcom.h>
+
+@interface GCObject (GCSwizzlingAdditions)
+
+@end
