f148d46f3791d92eab87d9ce9199ca37486724c9
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2013-04-25 11:54:55 +0200

    docs

--- Gedcom/NSObject+MELazyPropertySwizzlingAdditions.h
+++ Gedcom/NSObject+MELazyPropertySwizzlingAdditions.h
@@ -14,7 +14,7 @@
  
  * Include this file and its implementation.
  
- * #import this header and call +setupLazyProperties in +initialize in the classes you want to have lazies.
+ * `#import` this header and call `+setupLazyProperties` in `+initialize` in the classes you want to have lazies.
  
  * For each property that you wish to load lazily, implement the following lazy getter:
  
@@ -25,7 +25,7 @@
  }
  ```
  
- That's it, you're done. The first time a property is accessed, the _lazy getter is called in to set the ivar before calling the original implementation. Subsequently, a dispatch_once token is used to prevent the _lazy getter from running again.
+ That's it, you're done. The first time a property is accessed, the `_lazy` getter is called in to set the ivar before calling the original implementation. Subsequently, a dispatch_once token is used to prevent the `_lazy` getter from running again.
  
  */
 
