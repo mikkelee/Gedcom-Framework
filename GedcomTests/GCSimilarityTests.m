3a9be443977cbedc3e7e93f974846cdcac6bc9f8
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2013-11-21 13:18:36 +0100

    More detailed GCSimilarityTests

--- GedcomTests/GCSimilarityTests.m
+++ GedcomTests/GCSimilarityTests.m
@@ -27,6 +27,10 @@
 	[birt addAttributeWithType:@"date" value:[GCDate valueWithGedcomString:@"1 JAN 1901"]];
     [indi1.mutableProperties addObject:birt];
     
+	GCDeathAttribute *deat = [GCDeathAttribute death];
+	[deat addAttributeWithType:@"date" value:[GCDate valueWithGedcomString:@"12 SEP 1950"]];
+    [indi1.mutableProperties addObject:deat];
+    
 	GCIndividualRecord *indi2 = [GCIndividualRecord individualInContext:ctx];
 	[indi2 addAttributeWithType:@"personalName" value:[GCNamestring valueWithGedcomString:@"Jens /Hansen/ Smed"]];
 	[indi2 addAttributeWithType:@"sex" value:[GCGender maleGender]];
@@ -34,6 +38,10 @@
 	GCBirthAttribute *birt2 = [GCBirthAttribute birth];
 	[birt2 addAttributeWithType:@"date" value:[GCDate valueWithGedcomString:@"1901"]];
     [indi2.mutableProperties addObject:birt2];
+    
+	GCBirthAttribute *birt3 = [GCBirthAttribute birth];
+	[birt3 addAttributeWithType:@"date" value:[GCDate valueWithGedcomString:@"1902"]];
+    [indi2.mutableProperties addObject:birt3];
     
     GCSimilarity res1 = [indi1 similarityTo:indi2];
     GCSimilarity res2 = [indi2 similarityTo:indi1];
